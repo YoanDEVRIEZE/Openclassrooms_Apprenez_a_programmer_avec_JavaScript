80190964e925f8df7cbb7f1f7095475730e8c06a
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2024-09-28 17:24:22 +0200

    Ajout du bouton partager

--- scripts/afficherResultat.js
+++ scripts/afficherResultat.js
@@ -1,4 +1,25 @@
+function openForm() {
+    document.getElementById("popup-Form").style.display = "block";
+    document.getElementById("popupOverlay").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("popup-Form").style.display = "none";
+    document.getElementById("popupOverlay").style.display = "none"; 
+}
+
 function afficherResultat (score, listeChoisie) {    
-    let message = "Votre score est de : "+score + "/" + listeChoisie;
-    document.getElementById("scoreText").textContent = message;
+    if (!document.getElementById("boutonPart")) {
+        let message = "Votre score est de : "+score + "/" + listeChoisie;
+        document.getElementById("scoreText").textContent = message;
+        let partage = document.getElementById("score");
+        let div = document.createElement("div");
+        div.classList.add("partager");
+        let bouton = document.createElement("button");
+        bouton.id = ("boutonPart");
+        bouton.onclick = openForm;
+        bouton.textContent = "Partager le score";
+        div.appendChild(bouton);
+        partage.appendChild(div);
+    }
 }
