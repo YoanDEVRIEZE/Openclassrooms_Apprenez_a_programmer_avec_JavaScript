9cac5b456980ea07234be01dba665870f4842ec2
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2024-09-28 19:17:40 +0200

    Regroupement du code dans lancerLeJeu.js

--- scripts/lancerLeJeu.js
+++ scripts/lancerLeJeu.js
@@ -23,3 +23,60 @@ function lancerLeJeu() {
         }
     });
 }
+
+function validerLaSaisie() {
+    let saisie = document.getElementById("saisie").value;
+    
+    if (saisie != "") {
+        document.getElementById("erreur").textContent = "";
+        if (saisie == listeChoisie[currentIndex]) {
+            score++;
+        }
+
+        document.getElementById("saisie").value = "";
+        currentIndex++;
+
+        if (currentIndex < listeChoisie.length) {
+            document.getElementById("PhrasesMots").textContent = listeChoisie[currentIndex];
+        } else {
+            document.getElementById("PhrasesMots").textContent = "Fin du jeu !";
+            afficherResultat(score, listeChoisie.length);
+        }
+    } else {
+        document.getElementById("erreur").textContent = "Veuillez saisir du texte !";
+    }
+}
+
+function openForm() {
+    document.getElementById("popup-Form").style.display = "block";
+    document.getElementById("popupOverlay").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("popup-Form").style.display = "none";
+    document.getElementById("popupOverlay").style.display = "none"; 
+}
+
+function afficherResultat (score, listeChoisie) {    
+    if (!document.getElementById("boutonPart") && !document.getElementById("boutonRejouer")) {
+        document.getElementById("saisie").style.display = "none";
+        document.getElementById("bouton").style.display = "none";
+        document.getElementById("score").style.display = "table-cell";
+        let message = "Votre score est de : "+score + "/" + listeChoisie;
+        document.getElementById("scoreText").textContent = message;
+        let partage = document.getElementById("score");
+        let div = document.createElement("div");
+        div.classList.add("partager");
+        let bouton = document.createElement("button");
+        bouton.id = ("boutonPart");
+        bouton.onclick = openForm;
+        bouton.textContent = "Partager le score";
+        let boutonRejouer = document.createElement("button");
+        boutonRejouer.id = ("boutonRejouer");
+        boutonRejouer.onclick = lancerLeJeu;
+        boutonRejouer.textContent = "Rejouer !";
+        div.appendChild(bouton);
+        div.appendChild(boutonRejouer);
+        partage.appendChild(div);
+    }
+}
